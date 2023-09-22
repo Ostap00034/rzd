4a587d4eac12ba3f034db93c88aacfd5f881493f
Ostap Chichiginarov <ostap00034@gmail.com>
2023-09-22 23:55:17 +0900

    added tailwindcss

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,7 +1,7 @@
 function App(): JSX.Element {
   return (
-    <div className="container">
-      <h1>BEER</h1>
+    <div className="flex bg-red-400 w-full h-screen justify-center items-center flex-col">
+      <h1 className="text-[30px] font-bold">BEER</h1>
       lskdjflksdjfsduhfusidhfd
     </div>
   )
